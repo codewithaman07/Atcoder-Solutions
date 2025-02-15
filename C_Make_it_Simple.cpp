f5bb02b843fe23e1e3a8d1df277c322aa64d9b59
codewithaman07 <amansingh2009000@gmail.com>
2025-02-15 19:20:49 +0530

    AtCoder Beginner Contest 393

--- C_Make_it_Simple.cpp
+++ C_Make_it_Simple.cpp
@@ -11,6 +11,52 @@ using namespace __gnu_pbds;
 const int M = 1e9+7;
 const int N = 1e6 + 1;
 
+
+void solve() {
+    int n,m; cin>>n>>m;
+    map<pair<int,int>,int>mp;
+    int cnt = 0;
+    for(int i = 0; i<m; i++){
+        int u,v; cin>>u>>v;
+        if(u == v){
+            cnt++;
+            continue;
+        }
+        pair<int,int>e = {min(u,v), max(u,v)};
+        if(mp.count(e)) cnt++;
+        else mp[e]++;
+    }
+    cout<<cnt<<endl;
+}
+
+signed main() {
+    cout << fixed << setprecision(20);
+    Aman();
+
+    int tc = 1;
+
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
+
+
+
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define Aman() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl '\n'
+#define int long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+const int M = 1e9+7;
+const int N = 1e6 + 1;
+
 struct PairHash {
     size_t operator()(const pair<int, int>& p) const {
         return (long long)p.first * 31LL + p.second;
